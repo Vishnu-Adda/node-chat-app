64a58c6ea2e98fc0e1ef9596412be471ba6f42b0
Vishnu Adda <addavishnu@gmail.com>
2018-08-12 03:31:58 -0700

    Emit newMessage on createMessage.

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'croc@example.com',
-        text: 'Hey people'
-    });
 });
 
 socket.on('disconnect', function () {
