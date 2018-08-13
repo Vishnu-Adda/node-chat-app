75caa5ec0323bd2adcaec042cd6046dfead196a0
Vishnu Adda <addavishnu@gmail.com>
2018-08-12 20:19:08 -0700

    Add css for chat page.

--- public/js/index.js
+++ public/js/index.js
@@ -26,7 +26,7 @@ socket.on('newLocationMessage', function (message) {
     li.text(`${message.from}: `);
     a.attr('href', message.url);
     li.append(a);
-    
+
     jQuery('#messages').append(li);
 });
 
@@ -34,11 +34,13 @@ jQuery('#message-form').on('submit', function (e) {
     // Prevents default behavior for the event
     e.preventDefault();
 
+    var messageTextbox = jQuery('[name=message]');
+
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message]').val()
+        text: messageTextbox.val()
     }, function () {
-
+        messageTextbox.val('');
     });
 });
 
@@ -49,12 +51,16 @@ locationButton.on('click', function () {
         return alert('Geolocation not supported by your browser.');
     }
 
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
+
     navigator.geolocation.getCurrentPosition(function (position) {
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
     }, function () {
+        locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch location.');
     });
 });
