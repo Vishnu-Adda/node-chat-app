2c88d7842d369615f83b612502f4c84d9af5ff9e
Vishnu Adda <addavishnu@gmail.com>
2018-08-21 18:52:14 -0700

    Update users.js

--- server/utils/users.js
+++ server/utils/users.js
@@ -4,21 +4,18 @@
     room: 'The Office Fans'
 }]
 
-// addUser(id, name, room)
-// removeUser(id)
-// getUser(id)
-// getUserList(room)
-
 class Users {
     // constructors are optional
     constructor() {
         this.users = [];
     }
+//     Method to add a user to the room
     addUser(id, name, room) {
         var user = { id, name, room };
         this.users.push(user);
         return user;
     }
+//     Method to remove a user from the room
     removeUser(id) {
         var user = this.getUser(id);
 
@@ -28,9 +25,11 @@ class Users {
 
         return user;
     }
+//     Gets a user from the included user array
     getUser(id) {
         return this.users.filter((user) => user.id === id)[0];
     }
+//     Ability to list the users in the room
     getUserList(room) {
         var users = this.users.filter((user) => user.room === room);
         var namesArray = users.map((user) => user.name);
@@ -40,20 +39,3 @@ class Users {
 }
 
 module.exports = { Users };
-
-// class Person {
-//     constructor (name, age) {
-//         // 'this' refers to this specific instance
-//         this.name = name;
-//         this.age = age;
-//     }
-//     getUserDescription () {
-//         return `${this.name} is ${this.age} year(s) old.`;
-//     }
-// }
-
-// var me = new Person('Vishnu', 18);
-// var description = me.getUserDescription();
-// console.log(description);
-// console.log('this.name', me.name);
-// console.log('this.age', me.age);
